a332f3b2228cd8360569ee55261008e9ca0f141d
Iotogen <53600242+Iotogen@users.noreply.github.com>
2020-08-27 15:09:36 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+# Variables store data.
+# A variable can only hold one type of data at a time.
+# There are four basic data types in Python:
+# Integer for whole numbers, like 4 or 6.
+# String for text. (Note: Everything can be a string. Strings are always in quotation marks!)
+# Floating Point for decimal numbers, like 4.5 or 2.0
+# Boolean for True or False.
+
+# Declaring variables:
+# To declare a variable, you need to set its name and value.
+# (Note: Variable Names cannot include spaces. Name and value are separated by the equals ('=') character.
+
+# Examples:
+
+ a = 5
+ b = "Hello"
+ 
+ myFloat = 2.0
+ 
+ # Python will set the variable type automatically.
+ # If - for whatever reason - you would want to change a type, you can just write:
+ str(a)
+ or float(a)
+ 
+ # Note: When changing variables, note, that not every time can be converted into each other.
+ # For example: It is not possible to change a text to an integer.

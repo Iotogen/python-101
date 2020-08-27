44bd560ba300af3767d0ec1027c4fea59fe6e58b
Iotogen <53600242+Iotogen@users.noreply.github.com>
2020-08-27 14:15:16 +0200

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,8 @@
 # Hello World is the simplest program you can write in a programming language.
 # In Python, it looks like this:
 print("Hello World!")
+
+# How it works:
+# With the "print" statement, we can write a line to a console.
+# In the parentheses after the statement, we say what we want to be printed. In our case, it's the sentence "Hello World!".
+# We need to put quotation marks around our words, so Python knows, that this is a text.
